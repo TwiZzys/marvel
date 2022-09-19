8cde8cd959141d1e68b3527c279f335c6e7c7bfe
Vitalii <witaliy_sapriga@ukr.net>
2022-09-19 21:35:10 +0300

    Added Error and spinner

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,16 @@
+import img from './error.gif'
+const ErrorMessage = () => {
+    return (
+        <img src={img} alt="" style={
+            {
+                display: "block",
+                width: 250,
+                height: 250,
+                objectFit: 'contain',
+                margin: "0 auto"
+            }
+        }/>
+    )
+}
+
+export default ErrorMessage;
